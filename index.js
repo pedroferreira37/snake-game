f480fdbe2c669fa6508504571cf25aa4e74e2326
Pedro Ferreira <pedrohsferreira1@gmail.com>
2023-03-08 14:46:38 -0300

    refractored moves,render, states

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const snakeGame = () => {
       ],
       nextMove: null,
       direction: "ArrowRight",
-      speed: 100,
+      speed: 500,
     },
     food: [],
     points: 0,
@@ -29,7 +29,7 @@ const snakeGame = () => {
     const div = document.createElement("div");
     div.style.width = `${props.pixel}px`;
     div.style.height = `${props.pixel}px`;
-    div.style.background = "#747474";
+
     div.style.boxSizing = "border-box";
     div.style.position = `absolute`;
     div.style.top = `${props.x * props.pixel}px`;
@@ -54,6 +54,7 @@ const snakeGame = () => {
 
 function renderScreen(state) {
   const { board } = state;
+
   board.coordinates.forEach((boardHouse) => {
     screen.appendChild(boardHouse);
   });
@@ -61,20 +62,16 @@ function renderScreen(state) {
 
 function handleKeyPressed(event) {
   event.preventDefault();
-  state.nextMove = event.key;
+  state.snake.direction = event.key;
 }
 
 function removeKeyListener(handleKeyPressed) {
   window.removeEventListener("keydown", handleKeyPressed);
 }
 
-const maxValueConstraint = ([t, l]) => {
-  return [t, l].map((direction) => Math.min(30, Math.max(0, direction)));
-};
-
 const checkCollision = ([t, l]) => {
-  if (t < 0 || t > 30) return true;
-  else if (l < 0 || l > 30) return true;
+  if (t < 0 || t >= 40) return true;
+  else if (l < 0 || l >= 40) return true;
   else false;
 };
 
@@ -85,16 +82,16 @@ const acceptedMoves = {
       removeKeyListener();
       return false;
     }
-    return maxValueConstraint([t, l + 1]);
+    return [t, l + 1];
   },
 
-  ArrowRight([t, l]) {
+  ArrowLeft([t, l]) {
     const isCollide = checkCollision([t, l]);
     if (isCollide) {
       removeKeyListener();
       return false;
     }
-    return maxValueConstraint([t, l - 1]);
+    return [t, l - 1];
   },
 
   ArrowDown([t, l]) {
@@ -103,7 +100,7 @@ const acceptedMoves = {
       removeKeyListener();
       return false;
     }
-    return maxValueConstraint([t + 1, l]);
+    return [t + 1, l];
   },
 
   ArrowUp([t, l]) {
@@ -112,15 +109,50 @@ const acceptedMoves = {
       removeKeyListener();
       return false;
     }
-    return maxValueConstraint([t - 1, l]);
+    return [t - 1, l];
   },
 };
 
 function renderSnake(state) {
   const { snake } = state;
   const snakeBodyPosition = snake.coordinates.map(([x, y]) => `${x}-${y}`);
-  snakeBodyPosition.forEach((marker) => {
-    state.board.coordinates.get(marker).style.background = "yellow";
+  state.board.coordinates.forEach((boardHouse, marker) => {
+    if (snakeBodyPosition.includes(marker)) {
+      state.board.coordinates.get(marker).classList.add("snake");
+      state.board.coordinates.get(marker).classList.remove("board");
+    } else {
+      state.board.coordinates.get(marker).classList.remove("snake");
+      state.board.coordinates.get(marker).classList.add("board");
+    }
+  });
+}
+
+function moveSnake(snake) {
+  const snakeHead = snake.nextMove;
+  if (snakeHead) {
+    snake.coordinates.unshift(snakeHead);
+    snake.coordinates.pop();
+  }
+}
+
+function createRandomFood(state) {
+  const { snake, board } = state;
+  let foodRandomCoordinates = [
+    [
+      Math.floor(Math.random() * board.rows),
+      Math.floor(Math.random() * board.cols),
+    ],
+  ].map(([x, y]) => `${x}-${y}`);
+
+  state.snake.food = foodRandomCoordinates;
+}
+
+function renderFood(state) {
+  const { snake, board } = state;
+  board.coordinates.forEach((boardHouse, marker) => {
+    if (snake.food.includes(marker)) {
+      boardHouse.style.background = "green";
+    }
   });
 }
 
@@ -128,59 +160,31 @@ const { state } = snakeGame();
 
 renderScreen(state);
 renderSnake(state);
+createRandomFood(state);
+const gameInterval = setInterval(() => {
+  const { snake } = state;
+  if (snake.direction in acceptedMoves) {
+    snake.nextMove = acceptedMoves[snake.direction](snake.coordinates[0]);
+  }
 
-// const gameInterval = setInterval(() => {
-//   let snakeHead = state.snake[0];
+  if (snake.nextMove === false) {
+    clearInterval(gameInterval);
 
-//   if (state.direction in acceptedMoves) {
-//     state.nextMove = acceptedMoves[state.direction](snakeHead);
-//   }
+    return;
+  }
 
-//   if (state.nextMove === false) {
-//     clearInterval(gameInterval);
-//     return;
-//   }
+  moveSnake(snake);
+  renderSnake(state);
+  renderFood(state);
+}, 100);
 
-//   moveSnake(nextMove);
-//   drawSnake(snakeCoords);
-// }, state.speed);
+document.addEventListener("keydown", handleKeyPressed);
 
 // function showPoints() {
 //   score.innerHTML = POINTS + " points";
 // }
 
 //
-
-// function getInitialCoordsSnake() {
-//   return [
-//     [0, 5],
-//     [0, 4],
-//     [0, 3],
-//     [0, 2],
-//     [0, 1],
-//   ];
-// }
-
-// function drawSnake(coords) {
-//   const uniqueSnakePosition = new Set();
-//   for (const [x, y] of coords) {
-//     const cellPosition = retrivePosition(x, y);
-//     uniqueSnakePosition.add(cellPosition);
-//   }
-
-//   coordinates.forEach((cell, position) => {
-//     if (uniqueSnakePosition.has(position)) {
-//       cell.style.background = "yellow";
-//     }
-
-//     if (
-//       !uniqueSnakePosition.has(position) &&
-//       cell.style.background !== "green"
-//     ) {
-//       cell.style.background = "white";
-//     }
-//   });
-// }
 
 // function moveSnake(newHeadPosition) {
 //   const [f_x, f_y, cell] = foodCoords[0];
@@ -210,45 +214,6 @@ renderSnake(state);
 //   foodCoords.push([f_x, f_y, cell]);
 // }
 
-// const gameLoop = setInterval(() => {
-//   var newHeadPosition = snakeCoords[0];
-//   var nextMove;
-
-//   switch (nextDirection) {
-//     case "ArrowRight":
-//       nextMove = moveRight(newHeadPosition);
-//       break;
-//     case "ArrowLeft":
-//       nextMove = moveLeft(newHeadPosition);
-//       break;
-//     case "ArrowDown":
-//       nextMove = moveDown(newHeadPosition);
-//       break;
-//     case "ArrowUp":
-//       nextMove = moveUp(newHeadPosition);
-//       break;
-//     default:
-//       nextMove = moveRight(newHeadPosition);
-//   }
-
-//   if (nextMove === false) {
-//     clearInterval(gameLoop);
-//     return;
-//   }
-
 //   moveSnake(nextMove);
 //   drawSnake(snakeCoords);
 // }, SNAKE_SPEED);
-
-// function initGame() {
-//   snakeCoords = getInitialCoordsSnake();
-
-//   drawGame();
-//   drawSnake(snakeCoords);
-//   generateSnakeFood();
-//   showPoints();
-// }
-
-// const retrivePosition = (x, y) => `${x}:${y}`;
-
-// initGame();
